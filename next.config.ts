8923f9a6b06386bdbc4a467bb047c7298b8bbb79
Purav Sanghavi <sanghavipurav5@gmail.com>
2025-09-11 00:56:56 -0700

    ability to add agents to the database

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+   images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: 'https' as const,
+        hostname: 'api.dicebear.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+} 
 
 export default nextConfig;
